587547595d306c3b0a96dd8f88a2a0ada9c7c037
dongha <kdongha1103@nts-corp.com>
2021-03-14 22:32:45 +0900

    storybook webpack 세팅 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 module.exports = {
-    "stories": [
+    stories: [
         "../stories/**/*.stories.mdx",
         "../stories/**/*.stories.@(js|jsx|ts|tsx)",
         "../components/**/*.stories.@(js|jsx|ts|tsx)",
     ],
-    "addons": [
+    addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials"
-    ]
+    ],
+
+    webpackFinal: async (config) => {
+        [].push.apply(config.resolve.plugins,[
+            new TsconfigPathsPlugin({extensions: config.resolve.extensions})
+        ]);
+        return config;
+    }
 }
