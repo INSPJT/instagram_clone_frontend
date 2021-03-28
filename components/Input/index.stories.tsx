027e3091ff7cfe945d4641d64c19e206cebcb669
DESKTOP-V6KH5L6\목지애 <zhh102000@gmail.com>
2021-03-28 17:25:23 +0900

    [#38] 스토리북 추가

---
+++ components/Input/index.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Input, { InputType } from '.';
+
+
+export default {
+  title: 'Input',
+  component: Input,
+} as Meta;
+
+const Template: Story<InputType> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+};
