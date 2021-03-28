e209eecbb4dee122fa9e307c2cd178385371fe92
ssafy_kyz <biiregrey2@gmail.com>
2021-03-29 03:42:48 +0900

    [#39] add wider button for signin, signup

--- components/Button/index.stories.tsx
+++ components/Button/index.stories.tsx
@@ -13,36 +13,36 @@ const Template: Story<ButtonType> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  children: '팔로잉'
+  children: '팔로잉',
 };
 
 export const Blue = Template.bind({});
 Blue.args = {
   theme: 'blue',
-  children: '팔로우'
+  children: '팔로우',
 };
 
 export const DefaultLoading = Template.bind({});
 DefaultLoading.args = {
   loading: true,
-  children: '팔로우'
+  children: '팔로우',
 };
 
 export const BlueLoading = Template.bind({});
 BlueLoading.args = {
   loading: true,
   theme: 'blue',
-  children: '팔로우'
+  children: '팔로우',
 };
 
-export const DefaultSignin = Template.bind({});
-DefaultSignin.args = {
+export const DefaultSignup = Template.bind({});
+DefaultSignup.args = {
   children: '가입하기',
-  width: 'wide',  
+  width: 'wide',
 };
 
-export const BlueSignin = Template.bind({});
-BlueSignin.args = {
+export const BlueSignup = Template.bind({});
+BlueSignup.args = {
   theme: 'blue',
   children: '가입하기',
   width: 'wide',
