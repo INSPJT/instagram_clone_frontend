395cad9f1161f191ff0a57c6d1aa927346e93e1a
DESKTOP-V6KH5L6\목지애 <zhh102000@gmail.com>
2021-03-28 18:00:21 +0900

    [#39] 색상 변경, 타입 변경

--- components/Input/index.stories.tsx
+++ components/Input/index.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Input, { InputType } from '.';
+import Input, { InputType, Input2 } from '.';
 
 
 export default {
   title: 'Input',
-  component: Input,
+  component: Input2,
 } as Meta;
 
-const Template: Story<InputType> = (args) => <Input {...args} />;
+const Template: Story<InputType> = (args) => <Input2 {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
